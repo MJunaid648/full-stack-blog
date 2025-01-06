bc373b29f7252201c5ce96d222a04735d09fdfc8
Muhammad Junaid <648mjunaid@gmail.com>
2025-01-06 18:01:45 +0500

    setup router, layout and links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import Image from "./Image";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   return (
     <nav className="w-full h-16 md:h-20 flex items-center justify-between">
-      <div className="flex items-center gap-4 text-2xl font-bold">
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image src="logo.png" alt="blog logo" w={32} h={32} />
         <span>Lama logo</span>
-      </div>
+      </Link>
       {/* MOBILE MENU */}
       <div className="md:hidden">
         {/* MOBILE MENU BUTTON */}
@@ -22,29 +23,29 @@ const Navbar = () => {
           transition-all ease-in-out
           font-medium text-lg`}
         >
-          <a href="">Home</a>
-          <a href="">Trending</a>
-          <a href="">Most Popular</a>
-          <a href="">About</a>
-          <a href="">
+          <Link to="">Home</Link>
+          <Link to="">Trending</Link>
+          <Link to="">Most Popular</Link>
+          <Link to="">About</Link>
+          <Link to="">
             <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
               Login👋
             </button>
-          </a>
+          </Link>
         </div>
       </div>
 
       {/* MOBILE LINKS LIST */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="">Home</a>
-        <a href="">Trending</a>
-        <a href="">Most Popular</a>
-        <a href="">About</a>
-        <a href="">
+        <Link to="">Home</Link>
+        <Link to="">Trending</Link>
+        <Link to="">Most Popular</Link>
+        <Link to="">About</Link>
+        <Link to="">
           <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
             Login👋
           </button>
-        </a>
+        </Link>
       </div>
     </nav>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home.jsx";
+import PostList from "./routes/PostList.jsx";
+import SinglePost from "./routes/SinglePost.jsx";
+import Write from "./routes/Write.jsx";
+import Register from "./routes/Register.jsx";
+import Login from "./routes/Login.jsx";
+import MainLayout from "./Layouts/MainLayout.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <MainLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/posts", element: <PostList /> },
+      { path: "/:slug", element: <SinglePost /> },
+      { path: "/write", element: <Write /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
